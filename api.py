299950d3c338b2f76a41d18cf2a36a9107970057
FATESAIKOU <qzsecftbhhhh@gmail.com>
2020-09-17 00:01:05 +0900

    Add basic implement to connect to RDS

--- api.py
+++ api.py
@@ -1,9 +1,28 @@
+import json
+import pymysql
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route("/", methods=['GET'])
 def handler():
-    return 'hello world'
+    connection = pymysql.connect(
+        db='test',
+        port=3306,
+        host='localhost',
+        user='theusername',
+        password='thepassword'
+    )
+    
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    cursor.execute('SELECT * FROM comics')
+    data = cursor.fetchall()
+    cursor.close()
+    connection.close()
+
+    return json.dumps(data)
+
 
 if __name__ ==  '__main__':
+
     app.run(debug=True)
