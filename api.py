2fa8fb57b0dc4225344b9c43e62869936f2239bb
FATESAIKOU <qzsecftbhhhh@gmail.com>
2020-09-17 22:09:22 +0900

    Complete comic getting api

--- api.py
+++ api.py
@@ -4,25 +4,43 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/", methods=['GET'])
-def handler():
-    connection = pymysql.connect(
-        db='test',
-        port=3306,
-        host='localhost',
-        user='theusername',
-        password='thepassword'
-    )
-    
+connection = pymysql.connect(
+    db='test',
+    port=3306,
+    host='localhost',
+    user='theusername',
+    password='thepassword'
+)
+
+@app.route("/comics/", methods=['GET'])
+def getComics():
     cursor = connection.cursor(pymysql.cursors.DictCursor)
     cursor.execute('SELECT * FROM comics')
     data = cursor.fetchall()
     cursor.close()
-    connection.close()
+
+    return json.dumps(data)
+
+@app.route("/episodeof/<comic_id>", methods=['GET'])
+def getEpisodes(comic_id: str):
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    cursor.execute('SELECT * FROM episodes WHERE comic_id = %s', comic_id)
+    data = cursor.fetchall()
+    cursor.close()
+
+    return json.dumps(data)
+
+
+@app.route("/imageof/<episode_id>", methods=['GET'])
+def getImages(episode_id: str):
+    cursor = connection.cursor(pymysql.cursors.DictCursor)
+    cursor.execute('SELECT id, image_url FROM images WHERE episode_id = %s', episode_id)
+    data = cursor.fetchall()
+    cursor.close()
 
     return json.dumps(data)
 
 
 if __name__ ==  '__main__':
-
     app.run(debug=True)
+    connection.close()
